7b14fa5bfe23cf47b811a287540098b5b38b18bd
Matteo Abis <gmatteo.abis@gmail.com>
2012-12-12 16:23:21 +0100

    init.py

--- minimum_finder.py
+++ minimum_finder.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python
 from __future__ import division, print_function
+import numpy
+from skimage import io, transform
+from readimage import skimage_reader
+from readimage.skimage_reader import fromfile
 
-class MinimumFinder(object):
+class TipFinder(object):
     """
-    Find location of the minimum of the Y projection of the 2d histogram"""
-    def __init__(self, histogram, y_bin):
-        "histogram should be a TH2D"
+    Find location of the tip of the wire"""
+    def __init__(self, file_name):
         super(MinimumFinder, self).__init__()
-        self.histogram = histogram.ProjectionY("projectionY", y_bin, y_bin)
+        self.image = fromfile(file_name)
 
-    def get_minimum_bin(x_min, x_max):
-        self.histogram.SetRange(x_min, x_max)
-        return self.histogram.GetMinimumBin()
+    def get_wire_tip_coordinates():
+        "this should actually find the tip with the harris algorithm"
+        y, x = numpy.transpose(feature.harris(image, min_distance=100,
+            threshold=0.3, gaussian_deviation=5))
+        if len(x) != 1:
+            print("WARNING:", len(x), "tips found!")
+        x, y = x[0], y[0]
+        return x, y
+
+if __name__ == '__main__':
+    import sys
+    image = TipFinder(sys.argv[1])
+    print(image.get_wire_tip_coordinates())
